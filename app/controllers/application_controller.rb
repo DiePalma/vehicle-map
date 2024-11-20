424f23efc8d3b33dbeb6321d920a7e3fd214b601
DiePalma <diepalmafernandez@gmail.com>
2024-11-20 13:20:58 -0300

    cors fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+    after_action :set_cors_headers
+
+  private
+
+  def set_cors_headers
+    headers['Access-Control-Allow-Origin'] = request.headers['Origin']
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD'
+    headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
+  end
 end
